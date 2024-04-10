141ebaf8c70bffbd252b05b56c306385a8c8a032
denisprinchanu <127018735+denisprinchanu@users.noreply.github.com>
2024-04-10 05:07:03 -0400

    homework 3

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,84 +1,110 @@
+import flatpickr from 'flatpickr';
 
+import 'flatpickr/dist/flatpickr.min.css';
 
+import iziToast from 'izitoast';
 
-
-
-
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
 
 const input = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('[data-start]');
-const daysSpan = document.querySelector('[data-days]');
-const hoursSpan = document.querySelector('[data-hours]');
-const minutesSpan = document.querySelector('[data-minutes]');
-const secondsSpan = document.querySelector('[data-seconds]');
-let userSelectedDate = 0;
+const startBtn = document.querySelector('button[data-start]');
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSeconds = document.querySelector('[data-seconds]');
 
+startBtn.addEventListener('click', onClickStartTimer);
 startBtn.disabled = true;
 
-startBtn.addEventListener('click', onTimerStart);
+let timeDifference;
+let intervalId;
 
-flatpickr('input[type=text]', {
+const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-
   onClose(selectedDates) {
-    const currentDate = new Date();
-    if (selectedDates[0] - currentDate > 0) {
-      startBtn.disabled = false;
-    } else {
+    if (selectedDates[0] <= Date.now()) {
       startBtn.disabled = true;
-
       iziToast.error({
-        message: 'Please choose a date in the future!',
+        backgroundColor: 'tomato',
+        message: 'Please choose a date in the future',
+        messageColor: 'white',
+        messageSize: '20',
+        position: 'topRight',
+        close: true,
+        displayMode: 2,
       });
+    } else {
+      startBtn.disabled = false;
+      timeDifference = selectedDates[0];
     }
-    userSelectedDate = selectedDates[0];
   },
-});
+};
+
+flatpickr(input, options);
+
+function onClickStartTimer() {
+  intervalId = setInterval(calculateTimeLeft, 1000);
+  startBtn.disabled = true;
+  input.disabled = true;
+}
+
+function updateClockface(ms) {
+  const { days, hours, minutes, seconds } = convertMs(ms);
+
+  if (!days && !hours && !minutes && !seconds) {
+    clearInterval(intervalId);
+    input.disabled = false;
+  }
+
+
+
+
+
+
+  timerDays.textContent = addLeadingZero(days);
+  timerHours.textContent = addLeadingZero(hours);
+  timerMinutes.textContent = addLeadingZero(minutes);
+  timerSeconds.textContent = addLeadingZero(seconds);
+}
+
+
+function calculateTimeLeft() {
+  const currentDate = new Date().getTime();
+  const selectedDate = new Date(timeDifference).getTime();
+  const ms = selectedDate - currentDate;
+  updateClockface(ms);
+}
+
+
+
+
+
+
+
+
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
+  // Remaining days
   const days = Math.floor(ms / day);
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
-
-function addLeadingZero(value) {
-  return String(value).padStart(2, 0);
-}
-
-function updateTimerFields({ days, hours, minutes, seconds }) {
-  daysSpan.textContent = addLeadingZero(days);
-  hoursSpan.textContent = addLeadingZero(hours);
-  minutesSpan.textContent = addLeadingZero(minutes);
-  secondsSpan.textContent = addLeadingZero(seconds);
-}
-
-function onTimerStart() {
-  const selectedDate = userSelectedDate;
-  const timerId = setInterval(() => {
-    const startDate = new Date();
-    const countDown = selectedDate - startDate;
-    startBtn.disabled = true;
-    input.disabled = true;
-
-    if (countDown < 1000) {
-      clearInterval(timerId);
-      return;
-    }
-    updateTimerFields(convertMs(countDown));
-  }, 1000);
 }
